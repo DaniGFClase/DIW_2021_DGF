0c5c8310958e3637be026db2c6e97871bc57d447
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-01-12 18:22:16 +0100

    a

---
+++ Practica/Scripts/control_teclado.js
@@ -0,0 +1,22 @@
+/*var textBox = document.getElementById('instCoin');
+textBox.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "ArrowDown":
+            console.log("funca");
+            break;
+        case "ArrowUp":
+            console.log("funca");
+            break;
+        case "ArrowLeft":
+            console.log("funca");
+            break;
+        case "ArrowRight":
+            console.log("funca");
+            break;
+
+        default:
+            break;
+    }
+    console.log(`key=${event.key},code=${event.code}`);
+
+});*/
