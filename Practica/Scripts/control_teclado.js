973c7b9b82142ce7fde19f2a01cbb0857826c629
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-01-12 18:51:32 +0100

    a

--- Practica/Scripts/control_teclado.js
+++ Practica/Scripts/control_teclado.js
@@ -1,22 +1,55 @@
-/*var textBox = document.getElementById('instCoin');
-textBox.addEventListener('keydown', (event) => {
+var mando = document.getElementById('instCoin');
+mando.addEventListener('keydown', (event) => {
     switch (event.key) {
         case "ArrowDown":
-            console.log("funca");
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_atras.png')";
             break;
         case "ArrowUp":
-            console.log("funca");
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_alant.png')";
             break;
         case "ArrowLeft":
-            console.log("funca");
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_izq.png')";
             break;
         case "ArrowRight":
-            console.log("funca");
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_dcha.png')";
             break;
 
         default:
             break;
     }
     console.log(`key=${event.key},code=${event.code}`);
+});
 
-});*/
+mando.addEventListener('keyup', (event) => {
+    document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla.png')";
+    console.log(`key=${event.key},code=${event.code}`);
+
+});
+
+var botones = document.getElementById('instCoin');
+botones.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "a":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn1.png')";
+            break;
+        case "w":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn2.png')";
+            break;
+        case "e":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn3.png')";
+            break;
+        case "r":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn4.png')";
+            break;
+
+        default:
+            break;
+    }
+    console.log(`key=${event.key},code=${event.code}`);
+});
+
+mando.addEventListener('keyup', (event) => {
+    document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla.png')";
+    console.log(`key=${event.key},code=${event.code}`);
+
+});
