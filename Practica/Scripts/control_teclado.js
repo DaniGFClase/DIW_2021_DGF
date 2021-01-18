eca729295542fd571bc4ba27e4a910dd2d20fe8e
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-01-18 17:07:23 +0100

    a

--- Practica/Scripts/control_teclado.js
+++ Practica/Scripts/control_teclado.js
@@ -1,57 +1,47 @@
-function empezar() {
-    var mando = document.getElementById('instCoin');
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "ArrowDown":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_atras.png')";
+            break;
+        case "ArrowUp":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_alant.png')";
+            break;
+        case "ArrowLeft":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_izq.png')";
+            break;
+        case "ArrowRight":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_dcha.png')";
+            break;
 
-    mando.addEventListener('keydown', (event) => {
-        switch (event.key) {
-            case "ArrowDown":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_atras.png')";
-                break;
-            case "ArrowUp":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_alant.png')";
-                break;
-            case "ArrowLeft":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_izq.png')";
-                break;
-            case "ArrowRight":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_dcha.png')";
-                break;
+        default:
+            break;
+    }
+    console.log(`key=${event.key},code=${event.code}`);
+});
 
-            default:
-                break;
-        }
-        console.log(`key=${event.key},code=${event.code}`);
-    });
+document.addEventListener('keyup', (event) => {
+    document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla.png')";
+    console.log(`key=${event.key},code=${event.code}`);
 
-    mando.addEventListener('keyup', (event) => {
-        document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla.png')";
-        console.log(`key=${event.key},code=${event.code}`);
+});
 
-    });
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case "a":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn1.png')";
+            break;
+        case "w":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn2.png')";
+            break;
+        case "e":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn3.png')";
+            break;
+        case "r":
+            document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn4.png')";
+            break;
 
-    var botones = document.getElementById('instCoin');
-    botones.addEventListener('keydown', (event) => {
-        switch (event.key) {
-            case "a":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn1.png')";
-                break;
-            case "w":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn2.png')";
-                break;
-            case "e":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn3.png')";
-                break;
-            case "r":
-                document.getElementById('arcade').style.backgroundImage = "url('./CSS/Imagenes/Pantalla_btn4.png')";
-                break;
-
-            default:
-                break;
-        }
-        console.log(`key=${event.key},code=${event.code}`);
-    });
-
-}
-
-
-
-window.onload = empezar;
+        default:
+            break;
+    }
+    console.log(`key=${event.key},code=${event.code}`);
+});
--- Practica/scripts/pong_scr.js
+++ Practica/scripts/pong_scr.js
@@ -11,145 +11,150 @@ up1Pressed, down1Pressed -> control de la pala
 ptos1, ptos2 -> puntuaciones
 */
 
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width / 2;
-var y = canvas.height / 2;
-var dx = 1;
-var dy = -1;
-var paddleHeight = 75;
-var paddleWidth = 10;
-var paddleY = (canvas.height - paddleHeight) / 2;
-var up1Pressed = false;
-var down1Pressed = false;
+function empezar_Juego() {
 
-var paddle2Y = (canvas.height - paddleHeight) / 2;
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var ballRadius = 10;
+    var x = canvas.width / 2;
+    var y = canvas.height / 2;
+    var dx = 1;
+    var dy = -1;
+    var paddleHeight = 75;
+    var paddleWidth = 10;
+    var paddleY = (canvas.height - paddleHeight) / 2;
+    var up1Pressed = false;
+    var down1Pressed = false;
 
-var ptos1 = 0;
-var ptos2 = 0;
+    var paddle2Y = (canvas.height - paddleHeight) / 2;
+
+    var ptos1 = 0;
+    var ptos2 = 0;
 
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
 
-//J1 controles
-function keyDownHandler(e) {
-    if (e.keyCode == 40) {
-        up1Pressed = true;
-    } else if (e.keyCode == 38) {
-        down1Pressed = true;
-    }
-}
-
-function keyUpHandler(e) {
-    if (e.keyCode == 40) {
-        up1Pressed = false;
-    } else if (e.keyCode == 38) {
-        down1Pressed = false;
-    }
-}
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawPaddle1() {
-    ctx.beginPath();
-    ctx.rect(canvas.width - paddleWidth, paddleY, paddleWidth, paddleHeight);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawPaddle2() {
-    ctx.beginPath();
-    ctx.rect(0, paddle2Y, paddleWidth, paddleHeight);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function ptosIA() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("Score: " + ptos1, 8, 20);
-}
-
-function ptosIJ() {
-    ctx.font = "16px Arial";
-    ctx.fillStyle = "white";
-    ctx.fillText("Score: " + ptos2, canvas.width - 65, 20);
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    drawPaddle1();
-    drawPaddle2();
-    collisionDetection();
-    ptosIA();
-    ptosIJ();
-
-    //Rebotar arriba y abajo
-    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-        dy = -dy;
-    }
-    x += dx;
-    y += dy;
-
-    //controles J1
-    if (up1Pressed && paddleY < canvas.height - paddleHeight) {
-        paddleY += 2;
-    } else if (down1Pressed && paddleY > 0) {
-        paddleY -= 2;
-    }
-
-    //controles J2
-    if (dx < 0) {
-        if (y > (paddle2Y + paddleHeight) / 2 && paddle2Y < canvas.height - paddleHeight) {
-            paddle2Y += 2;
-        } else if (y < (paddle2Y + paddleHeight) / 2 && paddle2Y > 0) {
-            paddle2Y -= 2;
+    //J1 controles
+    function keyDownHandler(e) {
+        if (e.keyCode == 40) {
+            up1Pressed = true;
+        } else if (e.keyCode == 38) {
+            down1Pressed = true;
         }
     }
-}
 
-
-function collisionDetection() {
-    //Rebotar en las palas
-    if (dx > 0) {
-        if (x + dx < ballRadius) {
-            dx = -dx;
-        } else if (x + dx > canvas.width - ballRadius) {
-            if (y > paddleY && y < paddleY + paddleHeight) {
-                dx = -dx;
-            } else {
-                x = canvas.width / 2;
-                y = canvas.height / 2;
-                ptos1++;
-                console.log("ptos1 = " + ptos1);
-            }
+    function keyUpHandler(e) {
+        if (e.keyCode == 40) {
+            up1Pressed = false;
+        } else if (e.keyCode == 38) {
+            down1Pressed = false;
         }
-    } else {
-        if (x + dx > canvas.width - ballRadius) {
-            dx = -dx;
-        } else if (x + dx < ballRadius) {
-            if (y > paddle2Y && y < paddle2Y + paddleHeight) {
-                dx = -dx;
-            } else {
-                x = canvas.width / 2;
-                y = canvas.height / 2;
-                ptos2++;
-                console.log("ptos2 = " + ptos2);
+    }
+
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function drawPaddle1() {
+        ctx.beginPath();
+        ctx.rect(canvas.width - paddleWidth, paddleY, paddleWidth, paddleHeight);
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function drawPaddle2() {
+        ctx.beginPath();
+        ctx.rect(0, paddle2Y, paddleWidth, paddleHeight);
+        ctx.fillStyle = "white";
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    function ptosIA() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("Score: " + ptos1, 8, 20);
+    }
+
+    function ptosIJ() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "white";
+        ctx.fillText("Score: " + ptos2, canvas.width - 65, 20);
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBall();
+        drawPaddle1();
+        drawPaddle2();
+        collisionDetection();
+        ptosIA();
+        ptosIJ();
+
+        //Rebotar arriba y abajo
+        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+            dy = -dy;
+        }
+        x += dx;
+        y += dy;
+
+        //controles J1
+        if (up1Pressed && paddleY < canvas.height - paddleHeight) {
+            paddleY += 2;
+        } else if (down1Pressed && paddleY > 0) {
+            paddleY -= 2;
+        }
+
+        //controles J2
+        if (dx < 0) {
+            if (y > (paddle2Y + paddleHeight) / 2 && paddle2Y < canvas.height - paddleHeight) {
+                paddle2Y += 2;
+            } else if (y < (paddle2Y + paddleHeight) / 2 && paddle2Y > 0) {
+                paddle2Y -= 2;
             }
         }
     }
 
+
+    function collisionDetection() {
+        //Rebotar en las palas
+        if (dx > 0) {
+            if (x + dx < ballRadius) {
+                dx = -dx;
+            } else if (x + dx > canvas.width - ballRadius) {
+                if (y > paddleY && y < paddleY + paddleHeight) {
+                    dx = -dx;
+                } else {
+                    x = canvas.width / 2;
+                    y = canvas.height / 2;
+                    ptos1++;
+                    console.log("ptos1 = " + ptos1);
+                }
+            }
+        } else {
+            if (x + dx > canvas.width - ballRadius) {
+                dx = -dx;
+            } else if (x + dx < ballRadius) {
+                if (y > paddle2Y && y < paddle2Y + paddleHeight) {
+                    dx = -dx;
+                } else {
+                    x = canvas.width / 2;
+                    y = canvas.height / 2;
+                    ptos2++;
+                    console.log("ptos2 = " + ptos2);
+                }
+            }
+        }
+
+    }
+
+    setInterval(draw, 10);
 }
 
-setInterval(draw, 10);
+window.onload = empezar_Juego;
