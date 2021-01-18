0b0823aba042cd69aa7330930572168a4c8d8853
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-01-18 20:34:21 +0100

    Update meter_mondeda.js

--- Practica/scripts/meter_mondeda.js
+++ Practica/scripts/meter_mondeda.js
@@ -2,16 +2,19 @@ function meter_moneda() {
     var niv = 1;
     document.getElementById("insrtC").style.display = "none";
     document.getElementById("menu").style.display = "block";
-
+    document.getElementById("myCanvas").style.display = "none";
     document.getElementById("dicif").innerHTML = "Dificultad(min=1 max=7): " + niv;
 
 
     document.addEventListener('keydown', (event) => {
 
         if (event.key == "a") {
+            clearInterval(juego);
+
             empezar_Juego(niv);
         } else if (event.key == "r") {
             clearInterval(juego);
+            meter_moneda();
         } else if (event.key == "w") {
             niv++;
 
