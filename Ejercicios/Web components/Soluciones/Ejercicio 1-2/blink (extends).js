82aa273e6e2f01c3e4312ef3b6201b614ce1e29a
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-01-26 16:53:36 +0100

    Update blink (extends).js

--- Ejercicios/Web components/Soluciones/Ejercicio 1-2/blink (extends).js
+++ Ejercicios/Web components/Soluciones/Ejercicio 1-2/blink (extends).js
@@ -1,20 +1,20 @@
 class BlinkSpan extends HTMLSpanElement {
-    constructor () {
-      super ();      
+    constructor() {
+        super();
     }
-	
-	// Los atributos no existen hasta connectedCallback, por tanto no se deben acceder en el constructor
-	connectedCallback(){
-	  const interval = (this.getAttribute ('changeInterval') || 1) * 1000;
-      const base     = this.getAttribute ('baseColor') || 'inherit';
-      const alte     = this.getAttribute ('alternativeColor') || 'transparent';
-	  let n          = 0;	
-		
-	  setInterval (() => {
-        this.style.color = ++n % 2 ? alte : base;
-      }, interval);
-	}
+
+    // Los atributos no existen hasta connectedCallback, por tanto no se deben acceder en el constructor
+    connectedCallback() {
+        const interval = (this.getAttribute('changeInterval') || 1) * 1000;
+        const base = this.getAttribute('baseColor') || 'inherit';
+        const alte = this.getAttribute('alternativeColor') || 'transparent';
+        let n = 0;
+
+        setInterval(() => {
+            this.style.color = ++n % 2 ? alte : base;
+        }, interval);
+    }
 }
-  
+
 //Se define el elemento que extiende a span
-customElements.define ('wc-blink-span', BlinkSpan, {extends: 'span'});
+customElements.define('wc-blink-span', BlinkSpan, { extends: 'span' });
