2ce7b2f221985a20d96f3dbd272e3c5a4dcb8d1f
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-02-23 16:37:13 +0100

    a

---
+++ Práctica/prueba.js
@@ -0,0 +1,106 @@
+var pjs = document.getElementById('personaj');
+
+
+
+
+function tops() {
+    console.log(pjs);
+    pjs.innerHTML =
+        `<div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Top</div>
+        </div>`;
+}
+
+
+function mids() {
+    console.log(pjs);
+    pjs.innerHTML =
+        ` <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+        </div>`;
+}
+
+function jngls() {
+    console.log(pjs);
+    pjs.innerHTML =
+        ` <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+        </div>`;
+}
+
+function bots() {
+    console.log(pjs);
+    pjs.innerHTML =
+        ` <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+        </div>`;
+}
+
+function sups() {
+    console.log(pjs);
+    pjs.innerHTML =
+        ` <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+            </div>
+
+            <div class="pj">
+            <div class="pjImg"></div>
+            <div class="pjNomb">Nombre pj</div>
+        </div>`;
+}
