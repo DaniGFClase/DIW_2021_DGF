1c2ed15f0463fbaed6969f6521e11f4cb52b5549
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-02-23 15:36:50 +0100

    Update Ej11.js

--- Ejercicios/Ej11/Ej11.js
+++ Ejercicios/Ej11/Ej11.js
@@ -3,8 +3,6 @@ function mover() {
     var caja = document.getElementsByClassName('caja');
     console.log(caja[0]);
 
-    //   cajas[0].style.justifyItems = 'end';
-
     caja[0].style.transition = "all 5s";
     caja[0].style.transform = "translate(400px,120px)"
     caja[1].style.transition = "all 5s";
@@ -13,10 +11,6 @@ function mover() {
     caja[2].style.transform = "translate(400px,120px)"
     caja[3].style.transition = "all 5s";
     caja[3].style.transform = "translate(400px,-120px)"
-        // caja[0].style.gridArea = ' 2 / 1 / 3 / 2';
-        // caja[1].style.gridArea = ' 1 / 1 / 2 / 2';
-        // caja[2].style.gridArea = ' 4 / 1 / 5 / 2';
-        // caja[3].style.gridArea = ' 3 / 1 / 4 / 2';
 
 }
 
@@ -26,8 +20,6 @@ function volver() {
     var caja = document.getElementsByClassName('caja');
     console.log(caja[0]);
 
-    //   cajas[0].style.justifyItems = 'end';
-
     caja[0].style.transition = "all 5s";
     caja[0].style.transform = "initial"
     caja[1].style.transition = "all 5s";
@@ -36,9 +28,6 @@ function volver() {
     caja[2].style.transform = "initial"
     caja[3].style.transition = "all 5s";
     caja[3].style.transform = "initial"
-        // caja[0].style.gridArea = ' 2 / 1 / 3 / 2';
-        // caja[1].style.gridArea = ' 1 / 1 / 2 / 2';
-        // caja[2].style.gridArea = ' 4 / 1 / 5 / 2';
-        // caja[3].style.gridArea = ' 3 / 1 / 4 / 2';
+
 
 }
