7243c247c71b352a9978fb8888df240a1a7322eb
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-02-23 15:35:42 +0100

    Update Ej11.js

--- Ejercicios/Ej11/Ej11.js
+++ Ejercicios/Ej11/Ej11.js
@@ -29,13 +29,13 @@ function volver() {
     //   cajas[0].style.justifyItems = 'end';
 
     caja[0].style.transition = "all 5s";
-    caja[0].style.transform = "translate(-400px,-120px)"
+    caja[0].style.transform = "initial"
     caja[1].style.transition = "all 5s";
-    caja[1].style.transform = "translate(-400px,120px)"
+    caja[1].style.transform = "initial"
     caja[2].style.transition = "all 5s";
-    caja[2].style.transform = "translate(-400px,-120px)"
+    caja[2].style.transform = "initial"
     caja[3].style.transition = "all 5s";
-    caja[3].style.transform = "translate(-400px,120px)"
+    caja[3].style.transform = "initial"
         // caja[0].style.gridArea = ' 2 / 1 / 3 / 2';
         // caja[1].style.gridArea = ' 1 / 1 / 2 / 2';
         // caja[2].style.gridArea = ' 4 / 1 / 5 / 2';
