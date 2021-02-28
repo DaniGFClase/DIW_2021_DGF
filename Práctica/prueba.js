f53ee416f8dd95fa744dd563da0d057bd35e189f
DaniGFClase <60668304+DaniGFClase@users.noreply.github.com>
2021-02-28 14:12:17 +0100

    a

--- Práctica/prueba.js
+++ Práctica/prueba.js
@@ -6,101 +6,79 @@ var pjs = document.getElementById('personaj');
 function tops() {
     console.log(pjs);
     pjs.innerHTML =
-        `<div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Top</div>
-        </div>`;
+        `
+        <div class="videoNor">
+            <video width="320" height="240" controls>
+                <source src="CSS/Videos/Sion.mp4" type="video/mp4">
+                <source src="SS/Videos/Sion.ogv" type="video/ogv">
+            </video>
+        </div>
+        <div class="videoYt">
+            <iframe width="320" height="240" src="https://www.youtube.com/embed/EIktrvcTY9A?autoplay=1&mute=1"> </iframe>
+        </div>
+        `;
 }
 
 
 function mids() {
     console.log(pjs);
     pjs.innerHTML =
-        ` <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-        </div>`;
+        `       <div class="videoNor">
+        <video width="320" height="240" controls>
+            <source src="CSS/Videos/Akali.mp4" type="video/mp4">
+            <source src="SS/Videos/Akali.ogv" type="video/ogv">
+        </video>
+    </div>
+    <div class="videoYt">
+        <iframe width="320" height="240" src="https://www.youtube.com/embed/BuGd-85IoRY?autoplay=1&mute=1"> </iframe>
+    </div>`;
 }
+//https: //www.youtube.com/watch?v=BuGd-85IoRY
+
 
 function jngls() {
     console.log(pjs);
     pjs.innerHTML =
-        ` <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-        </div>`;
+        `    <div class="videoNor">
+        <video width="320" height="240" controls>
+            <source src="CSS/Videos/Ayuso.mp4" type="video/mp4">
+            <source src="SS/Videos/Ayuso.ogv" type="video/ogv">
+        </video>
+    </div>
+    <div class="videoYt">
+        <iframe width="320" height="240" src="https://www.youtube.com/embed/QnXep4UFdf4?autoplay=1&mute=1"> </iframe>
+    </div>`;
 }
+
+
 
 function bots() {
     console.log(pjs);
     pjs.innerHTML =
-        ` <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-        </div>`;
+        `     <div class="videoNor">
+        <video width="320" height="240" controls>
+            <source src="CSS/Videos/Senna.mp4" type="video/mp4">
+            <source src="SS/Videos/Senna.ogv" type="video/ogv">
+        </video>
+    </div>
+    <div class="videoYt">
+        <iframe width="320" height="240" src="https://www.youtube.com/embed/gr9vouiLqz0?autoplay=1&mute=1"> </iframe>
+    </div>`;
 }
 
 function sups() {
     console.log(pjs);
     pjs.innerHTML =
-        ` <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
+        `
+        <div class="videoNor">
+        <video width="320" height="240" controls>
+            <source src="CSS/Videos/Yuumi.mp4" type="video/mp4">
+            <source src="SS/Videos/Yuumi.ogv" type="video/ogv">
+        </video>
+    </div>
+    <div class="videoYt">
+        <iframe width="320" height="240" src="https://www.youtube.com/embed/KQQhGeFp4VE?autoplay=1&mute=1"> </iframe>
+    </div>
+        `;
 
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-            </div>
-
-            <div class="pj">
-            <div class="pjImg"></div>
-            <div class="pjNomb">Nombre pj</div>
-        </div>`;
 }
